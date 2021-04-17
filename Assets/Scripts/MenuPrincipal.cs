12b86d0534253ecfe2dac992a8237e4224f4c910
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-04-17 15:22:05 -0500

    Selección de personaje básica

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -6,11 +6,28 @@ using UnityEngine.UI;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    //Jugador seleccionó jugar, hace transición del menú a la selección de personajes
-    public void Jugar()
+
+    public GameObject continuar;
+
+    void Start()
     {
-        print("Cambiando de escena");
-        SceneManager.LoadScene("SeleccionPersonaje");
+        GameManager.jugador = 1;
+    }
+    
+    //Jugador seleccionó un botón que progresa el juego, hace transición de la escena actual a la siguiente
+    public void Avanzar()
+    {
+        print("Avanzando de escena");
+        GameManager.escena++;
+        SceneManager.LoadScene(GameManager.escena);
+    }
+
+    //Jugador seleccionó un botón que retrasa el juego, hace transición de la escena actual a la anterior
+    public void Retroceder()
+    {
+        print("Retrocediendo de escena");
+        GameManager.escena--;
+        SceneManager.LoadScene(GameManager.escena);
     }
 
     //Jugador seleccionó salir, el juego se cierra
@@ -18,5 +35,17 @@ public class MenuPrincipal : MonoBehaviour
     {
         print("Gracias por jugar");
         Application.Quit();
+    }
+
+    //Proceso de selección de personajes
+    public void SeleccionarPersonaje(int personaje)
+    {
+        print("Personaje Seleccionado");
+        GameObject boton = (GameObject) GameObject.Find("Canvas/Personaje"+personaje.ToString() + "/Text");
+        print("El jugador " + GameManager.jugador.ToString() + " seleccionó a " + boton.GetComponent<Text>().text);
+        GameManager.personajesSeleccionados[GameManager.jugador - 1] = personaje;
+        bool Continuar = (GameManager.jugador >= 2) ? true : false;
+        continuar.SetActive(Continuar);
+        GameManager.jugador += 1;
     }
 }
