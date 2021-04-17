0a7b04a0a30ecbd16e97203ec2378be7d0a9343e
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-04-17 16:29:17 -0500

    Seleccion de Personaje 2.0

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Linq;
 
 public class MenuPrincipal : MonoBehaviour
 {
 
     public GameObject continuar;
+    private GameObject anuncio;
+    private Text textoAnuncio;
+    public GameObject[] cambios = new GameObject[2];
 
     void Start()
     {
         GameManager.jugador = 1;
+        anuncio = (GameObject) GameObject.Find("Canvas/AnuncioJugador");
+        textoAnuncio = anuncio.GetComponent<Text>();
+        textoAnuncio.text = ("El jugador " + GameManager.jugador.ToString() + " está seleccionando personaje");
     }
     
     //Jugador seleccionó un botón que progresa el juego, hace transición de la escena actual a la siguiente
@@ -40,19 +47,32 @@ public class MenuPrincipal : MonoBehaviour
     //Proceso de selección de personajes
     public void SeleccionarPersonaje(int personaje)
     {
-        if (GameManager.jugador == 1 || GameManager.jugador == 2)
+        if (GameManager.personajesSeleccionados.Contains(0))
         {
             print("Personaje Seleccionado");
             GameObject boton = (GameObject)GameObject.Find("Canvas/Personaje" + personaje.ToString() + "/Text");
             print("El jugador " + GameManager.jugador.ToString() + " seleccionó a " + boton.GetComponent<Text>().text);
             GameManager.personajesSeleccionados[GameManager.jugador - 1] = personaje;
-            bool Continuar = (GameManager.jugador >= 2) ? true : false;
-            continuar.SetActive(Continuar);
+            bool Continuar = GameManager.personajesSeleccionados.Contains(0);
+            continuar.SetActive(!Continuar);
+            cambios[GameManager.jugador - 1].SetActive(true);
             GameManager.jugador += 1;
+            textoAnuncio.text = (GameManager.personajesSeleccionados.Contains(0)) ? ("El jugador " + GameManager.jugador.ToString() + " está seleccionando personaje") : ("Personajes seleccionados");
         }
         else
         {
+            continuar.SetActive(true);
+            textoAnuncio.text = ("Personajes seleccionados");
             print("No puedes seleccionar más");
         }
+    }
+
+    public void CambiarPersonaje(int jugador)
+    {
+        print("El jugador " + jugador.ToString() + " va a cambiar de personaje");
+        GameManager.jugador = jugador;
+        GameManager.personajesSeleccionados[GameManager.jugador - 1] = 0;
+        continuar.SetActive(false);
+        textoAnuncio.text = ("El jugador " + GameManager.jugador.ToString() + " está seleccionando personaje");
     }
 }
