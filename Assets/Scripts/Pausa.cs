81007b3a3ce8f34d4d1e3c5076b9e86dd33c9262
luisferro2 <a01378248@itesm.mx>
2021-04-18 00:16:47 -0500

    musica y pausa
    Pongo la musica en todos lados y pongo la pausa en la escena de combte.

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// Contiene las funciones que hacen que el pausa funcione, script asociado al canvas.
+
+public class Pausa : MonoBehaviour
+{
+    public AudioSource musicaFondo;
+    // Musica de pausa.
+    public AudioSource musicaPausa;
+
+    public void Pausar() {
+        musicaFondo.Stop();
+        this.transform.GetChild(5).gameObject.SetActive(true);
+        musicaPausa.Play();
+        Time.timeScale = 0;
+    }
+
+    public void CambiarAMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Continuar() {
+        musicaPausa.Stop();
+        musicaFondo.Play();
+        this.transform.GetChild(5).gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Start()
+    {
+        // Desactivar el panel de pausa el inicio.
+        this.transform.GetChild(5).gameObject.SetActive(false);
+    }
+}
